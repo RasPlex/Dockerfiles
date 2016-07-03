7c6d97bf415432a0292b92818ba1efe238769b99
Jonas Karlman <jonas@kwiboo.se>
2016-07-03 12:57:30 +0200

    update debian files

--- debian/openpht.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-PHTROOT=/usr/lib/openpht
-export LD_LIBRARY_PATH=$PHTROOT/bin/system/players/dvdplayer:$LD_LIBRARY_PATH
-export XBMC_HOME=$PHTROOT/share/XBMC
-$PHTROOT/bin/plexhometheater
